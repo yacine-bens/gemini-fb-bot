2c0d7ac0a4b0c64e613f12e4ba64d03b6b547cc6
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2024-05-04 22:28:15 +0100

    Updated PORT

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,6 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log('Server is running on port 4000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
