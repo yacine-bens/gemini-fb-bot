dcd69ba9d9721ba4ea11f6b5f457de74dbddf609
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2024-05-04 22:27:10 +0100

    Updated PORT

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 4000, () => {
+  console.log('Server is running on port 4000');
 });
