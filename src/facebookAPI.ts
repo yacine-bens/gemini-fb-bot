fa23df89e5d1af93a44510f4345f02e094570d1c
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2024-05-05 01:52:02 +0100

    Added image handling

--- src/facebookAPI.ts
+++ src/facebookAPI.ts
@@ -1,5 +1,5 @@
 import { transcribeUrl } from "./deepgram.js";
-import { geminiPro } from "./geminipro.js";
+import { geminiPro, geminiProVision } from "./geminipro.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,26 +8,39 @@ const { PAGE_ACCESS_TOKEN } = process.env;
 const handleMessage = async (sender_psid: string, received_message: any) => {
     const { text, attachments } = received_message;
 
-    if (attachments && attachments[0] && attachments[0].type === 'audio') {
+    if (attachments && attachments[0]) {
         await callSendAPI(sender_psid, 'Please wait...');
-
+        
         const { payload } = attachments[0];
-        if (payload && payload.url) {
-            const { url } = payload;
-            const response = await transcribeUrl(url);
-            if (!response) return;
-            try {
-                const { transcript } = response?.results.channels[0].alternatives[0];
+        if (!payload || payload.url) return;
+
+        switch (attachments[0].type) {
+            case 'audio':
+                const response = await transcribeUrl(payload.url);
+                if (!response) return;
+                try {
+                    const { transcript } = response?.results.channels[0].alternatives[0];
+                    // split large message into smaller messages
+                    const messages = transcript.match(/.{1,1800}/g)!;
+                    for (const message of messages) {
+                        await callSendAPI(sender_psid, message);
+                    }
+                }
+                catch (error) {
+                    console.error(error);
+                }
+                break;
+            case 'image':
+                const base64image = await getBase64(payload.url);
+                const visionResponse = await geminiProVision(base64image, `I can't see the image. Please describe it.`);
                 // split large message into smaller messages
-                const messages = transcript.match(/.{1,1800}/g)!;
-                for (const message of messages) {
+                const visionMessages = visionResponse.match(/.{1,1800}/g)!;
+                for (const message of visionMessages) {
                     await callSendAPI(sender_psid, message);
                 }
-            }
-            catch (error) {
-                console.error(error);
-            }
+                break;
         }
+
     }
     else if (text) {
         await callSendAPI(sender_psid, 'Please wait...');
@@ -58,6 +71,12 @@ const callSendAPI = async (sender_psid: string, response: any) => {
         },
         body: JSON.stringify(request_body)
     });
+};
+
+const getBase64 = async (url: string) => {
+    const response = await fetch(url);
+    const buffer = await response.arrayBuffer();
+    return Buffer.from(buffer).toString('base64');
 };
 
 export { handleMessage };
